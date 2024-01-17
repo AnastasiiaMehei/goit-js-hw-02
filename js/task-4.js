079245a02f58f2ba31f6582c4c3941038ef45b3c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-01-17 23:24:55 +0200

    2

--- js/task-4.js
+++ js/task-4.js
@@ -4,25 +4,25 @@ function getShippingCost(country) {
     case 1:
       country = China;
       price = 100;
-      return 'Shipping to ${country} will cost ${price} credits';
+      return `Shipping to ${country} will cost ${price} credits`;
       break;
     case 2:
       country = Chile;
       price = 250;
-      return 'Shipping to ${country} will cost ${price} credits';
+      return '';
       break;
     case 3:
       country = Australia;
       price = 170;
-      return 'Shipping to ${country} will cost ${price} credits';
+      return `Shipping to ${country} will cost ${price} credits`;
       break;
     case 3:
       country = Jamaica;
       price = 120;
-      return 'Shipping to ${country} will cost ${price} credits';
+      return `Shipping to ${country} will cost ${price} credits`;
       break;
     default:
-      return 'Sorry, there is no delivery to your country';
+      return `Sorry, there is no delivery to your country`;
   }
-  return getShippingCost;
+  return getShippingCost(country);
 }
