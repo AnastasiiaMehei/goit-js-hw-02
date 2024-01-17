f610bfb76f353051093f97f8bdcce7f4a52740fd
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-01-17 21:27:35 +0200

    1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+  customerCredits >= totalPrice;
+  if (totalPrice !== customerCredits) {
+    return `Insufficient funds!`;
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
+  return makeTransaction;
+}
